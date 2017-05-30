18a5a30396691c52c1f6d2c1fa23080a75797459
nikson <nikson_kanti.paul@mailbox.tu-dresden.de>
2017-05-30 17:25:57 +0200

    Update turtlebot coordinate for both ros and bespoon

--- scripts/turtle_location.py
+++ scripts/turtle_location.py
@@ -4,26 +4,48 @@ import rospy
 import traceback
 from std_msgs.msg import String 
 from geometry_msgs.msg import PoseStamped, Pose
+from bespoon_pub_marker import Bespoon 
 
+class Turtle(object):
 
-def callback(data):
-    #rospy.loginfo(rospy.get_caller_id() + 'Data=%s', data)
-    ros_xyz = [data.position.x, data.position.y, data.position.z]
-    # ROS position     
-    data = {}
-    data['ros'] = ros_xyz
-    rospy.loginfo(data)
+    __bsp = Bespoon()
 
+    def __init__(self):
+        rospy.init_node('turtle_location_1', anonymous=True)
+        self.bespoon_data=None
+        self.ros_data=None
+        self.location=dict()        
+        pass 
+    
+    def callback(self, data):
+        global bsp
+        #rospy.loginfo(rospy.get_caller_id() + 'Data=%s', data)
+        if isinstance(data, Pose): 
+            # ROS position              
+            self.ros_data = data      
+            ros_xyz = [data.position.x, data.position.y, data.position.z]        
+            self.location['ros'] = ros_xyz
+        else: 
+            # Bespoon data             
+            self.bespoon_data = self.__bsp.format_data_for_marker(data.data)
+            if self.bespoon_data.has_key('anchor'):  
+                self.location['anchor'] = self.bespoon_data.get('anchor')
+        # write to ros log             
+        rospy.loginfo(self.location)
 
-def subscribe():
-    rospy.init_node('turtle_location_1', anonymous=True)
-    rospy.Subscriber('tf_map_transform', Pose, callback)
-    rospy.spin()
+    def subscribe(self):        
+        rospy.Subscriber('tf_map_transform', Pose, self.callback)
+        rospy.spin()
+
+    def subscribe2(self):        
+        rospy.Subscriber('bespoon', String, self.callback)        
 
 if __name__ == '__main__':
     try:
         print 'Starting turtle_location tracker'
-        subscribe()
+        turtle = Turtle()
+        turtle.subscribe2()
+        turtle.subscribe()
     except Exception as e:
         print("Error: ", e)
         print traceback.print_exc()
