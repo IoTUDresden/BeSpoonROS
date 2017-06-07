e5aecd790e11b769164ee3928871991bcfd26be7
nikson <nikson_kanti.paul@mailbox.tu-dresden.de>
2017-06-07 18:31:32 +0200

    Added coordinate converter test program

---
+++ scripts/my_cord_converter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python 
+
+from pyTrilater import cord_transform as ct 
+
+def read_xy():
+    x = input("Enter X:")
+    y = input("Enter Y:")
+    return (x,y)
+
+
+def main(): 
+    print 'ROS-MyCord Conversion'
+    sf = input('Scale: ')
+    while True:
+        c = input("Select (1. ROS-to-MyCord, 2. MyCord-to-ROS, 3. Quit): ")
+        if c == 3: 
+            break 
+        elif c == 1: 
+            xy = list(read_xy())                         
+            cxy = ct.get_my_custom_xy_from_rxy(xy, sf)
+            print cxy
+        elif c == 2:
+            xy = list(read_xy())
+            rxy = ct.get_ros_xy_from_custom_xy(xy, sf)
+            print rxy
+        
+
+if __name__ == '__main__':
+    main()
+    
