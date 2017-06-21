dd29f41f0ece0f9ec55b2a31b96310fd4b999cba
nikson <nikson_kanti.paul@mailbox.tu-dresden.de>
2017-06-21 15:55:04 +0200

    Update simple location publisher

--- scripts/turtle_location.py
+++ scripts/turtle_location.py
@@ -3,9 +3,8 @@
 import rospy
 import traceback
 from std_msgs.msg import String 
-from geometry_msgs.msg import PoseStamped, Pose
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 from bespoon_pub_marker import Bespoon 
-from rosdata import RosData 
 from pyTrilater import cord_transform as ct 
 
 class Turtle(object):
@@ -15,22 +14,19 @@ class Turtle(object):
     def __init__(self):        
         self.ros_data=None
         self.bespoon_data=None        
-        self.rxy = list()       # turtle current position from map 
-        self.bxy = list()       # anchor current position from bespoon
+        self.rxy = list()       # turtle current location from map 
+        self.bxy = list()       # bespoon anchor location from bespoon
                     
-    def callback(self, data):        
-        #rospy.loginfo(rospy.get_caller_id() + 'Data=%s', data)
-
+    def callback(self, data):                
         if isinstance(data, Pose): 
-            # turtle ros position data
+            # turtle ros location 
             self.ros_data = data      
             self.rxy = [data.position.x, data.position.y, data.position.z]                    
         else: 
-            # anchor position data 
+            # anchor location  
             self.bespoon_data = self.__bsp.format_data_for_marker(data.data)
             if self.bespoon_data.has_key('anchor'):  
-                self.bxy = self.bespoon_data.get('anchor')             
-        # rospy.loginfo()
+                self.bxy = self.bespoon_data.get('anchor')                     
         # publish all data again 
         self.publish()
 
@@ -42,24 +38,29 @@ class Turtle(object):
         rospy.Subscriber('bespoon', String, self.callback)
         # rospy.spin()       
 
-    def position_publisher(self):
-        rospy.init_node('all_position_node', anonymous=True)
-        self.topic = rospy.Publisher('current_positions', String)  
+    def simple_coord_publisher(self):
+        rospy.init_node('simple_position_node', anonymous=True)        
+        self.topic_simple_ros = rospy.Publisher('simpleRosLocation', Pose)  
+        self.topic_simple_anchor = rospy.Publisher('simpleBeSpoonLocation', Pose)  
 
     def publish(self):
-        data = RosData()
-        data.turtle_ros_position = self.rxy 
-        data.anchor_ros_position = self.bxy 
-        data.turtle_custom_position = ct.get_simple_xy_from_ros_xy(self.rxy) if len(self.rxy) > 1 else list()
-        data.anchor_custom_position = ct.get_simple_xy_from_ros_xy(self.bxy) if len(self.bxy) > 1 else list()
-        # rospy.loginfo(data.toJson())        
-        self.topic.publish(data.toJson())
+        # Use simple_xy conversion 
+        simple_rxy = ct.get_simple_xy_from_ros_xy(self.rxy) if len(self.rxy) > 1 else [None, None, None]
+        simple_bxy = ct.get_simple_xy_from_ros_xy(self.bxy) if len(self.bxy) > 1 else [None, None, None]
+
+        rpose = Pose( Point (simple_rxy[0], simple_rxy[1], simple_rxy[2]) , self.ros_data.orientation)
+        bpose = Pose( Point (simple_bxy[0], simple_bxy[1], simple_bxy[2]) , Quaternion(0,0,0,1))
+
+        # publish pose information in simple coordinate 
+        self.topic_simple_ros.publish(rpose)
+        self.topic_simple_anchor.publish(bpose)
+
 
 if __name__ == '__main__':
     try:
-        print 'Starting turtlebot position publisher'
+        print 'Starting turtlebot simple location publisher'
         turtle = Turtle()
-        turtle.position_publisher()
+        turtle.simple_coord_publisher()
         turtle.subscribe_ros_position()
         turtle.subscribe_anchor_position()
         rospy.spin()
