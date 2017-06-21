0b009fc4905167fb4cf8fedffa3fd24b01168462
nikson <nikson_kanti.paul@mailbox.tu-dresden.de>
2017-06-21 16:48:03 +0200

    Change lookup property name for bespoon jar

--- scripts/bespoon_java_server.py
+++ scripts/bespoon_java_server.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python 
 
-"""
-BeSpoon(java) anchor tracker run as ROS python topic and publish 
-
-set CLASSPATH=<bespoon-java-jar-path>
-
-"""
-
+import os
+import subprocess
 import rospy
 from std_msgs.msg import String
-import subprocess
-import os
 
 default_file = "bespoon.jar"
 
+"""
+Start the bespoon tracker project in separate process and pipe output data. 
+
+N.B.: 
+We could publish it as Java ROS Node, we just wanted to avoid setup ros_java in our current configuration 
+"""
+
 def bespoon():
+    rospy.init_node('bespoon_server', anonymous=True)    
     pub = rospy.Publisher('bespoon', String)
-    # queue_size not supported in groovy 
-    #pub = rospy.Publisher('bespoon', String, queue_size=10)    
-    rospy.init_node('bespoon_server', anonymous=True)
+    # refresh rate     
     rate = rospy.Rate(10) # 0.1 = 10hz/100
     cmd = ['java', '-jar']
 
     # add java class path and jar file 
-    jarfile_with_path = os.getenv('FILE_NAME')
+    jarfile_with_path = os.getenv('BESPOON_JAR_FILE')
 
-    if jarfile_with_path != None:       
+    if jarfile_with_path is not None:       
         cmd.append(jarfile_with_path)
     else:
         cmd.append(default_file)
-    ###########
-
+    
     # init a new proceess for bespoon
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     print("Process id: ", proc.pid)
@@ -48,5 +46,5 @@ def bespoon():
 if __name__ == "__main__":    
     try:
         bespoon()
-    except rospy.ROSInterruptException:
-        pass
+    except rospy.ROSInterruptException as e:
+        print("Error: ", e)
